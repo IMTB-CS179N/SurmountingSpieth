682b05e2b78b3c039c363f29f51be3eed1ef6ca2
susan2099 <59705471+susan2099@users.noreply.github.com>
2023-04-16 19:27:37 -0700

    checking if commit works

---
+++ Assets/Scripts/Ability.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Character Abilities")]
+
+public class Abilities
+{
+    public int manaCost;
+    
+}
