d9ead90917c592be54447837f0119ab843a34710
susan2099 <59705471+susan2099@users.noreply.github.com>
2023-05-02 23:55:16 -0700

    minor change to test csv

--- Assets/Scripts/Ability.cs
+++ Assets/Scripts/Ability.cs
@@ -1,41 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
-
 using UnityEngine;
+using System;
 
 public class Ability : MonoBehaviour
 {
-    public string abilityName {get; set;}
-    public string abilityDescription {get; set;}
-    public int manaCost {get; set;}
-    [SerializeField] 
+    public Stats stats = new Stats();
+    public string ClassName{get;set;}
+    public string AbilityName {get; set;}
+    //public string abilityDescription {get; set;}
+    public int ManaCost {get; set;}
+    public int DamageMultiplier {get; set;}
+    
+    [SerializeField]
     private int currentMana;
+
+    private int currentDamage;
     public GameObject projectile;
     public float projectileForce;
-    public Stats stats = new Stats();
-
 
     public Ability() {
-        abilityName = "";
-        abilityDescription = "";
-        manaCost = 5;
+        AbilityName = "Slash";
+        //abilityDescription = "";
+        ManaCost = 5;
+        DamageMultiplier = 1;
+        ClassName = stats.C_Class;
         stats.BaseDamage = 2;
         stats.BaseMana = 20;
     }
 
-    public void manaReduction(int manaRed) {
-        currentMana -= manaRed;
+    public void manaReduction(int manaCost) {
+        currentMana -= manaCost;
+    }
+    
+    public void abilityDamageIncrease(int damageMultiplier) {
+        currentDamage *= damageMultiplier;
     }
 
-    void attackAbility() {
-        manaReduction(manaCost);
+    public void attackAbility() {
+        manaReduction(ManaCost);
+        abilityDamageIncrease(DamageMultiplier);
         GameObject spell = Instantiate(projectile, transform.position, Quaternion.identity);
         Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         Vector2 myPos = transform.position;
         Vector2 direction = (mousePos - myPos).normalized;
         spell.GetComponent<Rigidbody2D>().velocity = direction * projectileForce;
     }
-
+    //public void Dispose(){}
     void Update() {
         if(Input.GetMouseButtonDown(0)) {
             if(currentMana < 5) {
@@ -50,6 +61,7 @@ public class Ability : MonoBehaviour
         }
     }
     void Start() {
+        currentDamage = stats.BaseDamage;
         currentMana = stats.BaseMana;
     }
 }
