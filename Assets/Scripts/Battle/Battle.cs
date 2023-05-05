f483da375e62fd2bf02eab5b37a85608d4c85dea
hscottvo <56327086+hscottvo@users.noreply.github.com>
2023-05-05 16:52:53 -0700

    Added basic battle setup

---
+++ Assets/Scripts/Battle/Battle.cs
@@ -0,0 +1,94 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Battle : MonoBehaviour
+{
+    public enum StateType
+    {
+        Start,
+        Player,
+        Enemy
+    }
+
+    private static Battle ms_instance;
+
+    public static Battle Instance =>
+        ms_instance == null ? (ms_instance = FindFirstObjectByType<Battle>()) : ms_instance;
+
+    private List<Enemy> m_enemies;
+
+    private StateType CurrentTurn = StateType.Player;
+
+    // Start is called before the first frame update
+    void Start() { }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (CurrentTurn)
+        { //transitions
+            case StateType.Player:
+                if (PlayerFinished())
+                {
+                    this.PerformPlayerAttack();
+                    CurrentTurn = StateType.Enemy;
+                }
+
+                break;
+            case StateType.Enemy:
+                if (EnemyFinished())
+                {
+                    CurrentTurn = StateType.Player;
+                }
+                break;
+        }
+
+        switch (CurrentTurn)
+        { //actions
+            case StateType.Player:
+
+                break;
+
+            case StateType.Enemy:
+
+                break;
+        }
+    }
+
+    private bool PlayerFinished()
+    {
+        //if UI button got clicked if animation finished
+        return InputProcessor.IsButtonPressed(Key.C);
+    }
+
+    private bool EnemyFinished()
+    {
+        return InputProcessor.IsButtonPressed(Key.V);
+    }
+
+    private void PerformPlayerAttack()
+    {
+        var enemy = this.m_enemies[0];
+
+        if (BattleUtil.Attack(Player.Instance, enemy))
+        {
+            this.m_enemies.RemoveAt(0);
+
+            if (this.m_enemies.Count == 0)
+            {
+                // win
+            }
+            else
+            {
+                // update ui and battlefield
+            }
+        }
+    }
+
+    public void ResetBattle()
+    {
+        CurrentTurn = StateType.Player;
+        // reset monster list
+    }
+}
---
+++ Assets/Scripts/Entity/Entity.cs
@@ -0,0 +1,106 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Project
+{
+    public interface IEntity
+    {
+        int Health { get; set; }
+        int Mana { get; set; }
+
+        int Damage { get; }
+
+        int Precision { get; }
+        float DodgeChance { get; }
+
+        int ComputeDamage();
+    }
+
+    public class Player : IEntity
+    {
+        public static Player Instance = new();
+
+        private Stats m_stats;
+        private Ability[] m_abilities;
+
+        // private List<Weapon> m_weapons;
+        // private List<Armor> m_armors;
+        private int m_precision;
+        private float m_dodgeChance;
+
+        public Player(Stats stats)
+        {
+            this.m_stats = stats;
+
+            Health = stats.Health;
+        }
+
+        public int Health { get; set; }
+        public int Mana { get; set; }
+
+        public int Damage { get; set; }
+
+        public int Precision => this.m_precision;
+        public float DodgeChance => this.m_dodgeChance;
+
+        public int ComputeDamage()
+        {
+            return 0;
+        }
+    }
+
+    public class SomeClass
+    {
+        private Stats m_stats;
+
+        public int CurrentHealth;
+
+        public SomeClass(Stats stats)
+        {
+            this.m_stats = stats;
+        }
+    }
+
+    public class Enemy : IEntity
+    {
+        private int m_precision;
+        private float m_dodgeChance;
+
+        public int Health { get; set; }
+        public int Mana { get; set; }
+
+        public int Damage { get; set; }
+
+        public int Precision => this.m_precision;
+        public float DodgeChance => this.m_dodgeChance;
+
+        public int ComputeDamage()
+        {
+            return 0;
+        }
+    }
+
+    public static class BattleUtil
+    {
+        public static bool Attack(IEntity attacker, IEntity receiver)
+        {
+            if (CheckIfHit(attacker, receiver))
+            {
+                receiver.Health -= attacker.ComputeDamage();
+
+                if (receiver.Health <= 0)
+                {
+                    return true; // kill
+                }
+            }
+
+            return false;
+        }
+
+        public static bool CheckIfHit(IEntity attacker, IEntity receiver)
+        {
+            return true; // take into account precision and dodge chance, return true if hit
+        }
+    }
+}
