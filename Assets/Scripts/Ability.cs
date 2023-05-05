dffb44b1e2975a3eec1ba16a28c7bdd875797fbc
susan2099 <59705471+susan2099@users.noreply.github.com>
2023-05-04 22:50:39 -0700

    cooldown logic

--- Assets/Scripts/Ability.cs
+++ Assets/Scripts/Ability.cs
@@ -12,6 +12,8 @@ public class Ability : IDisposable
     //public string abilityDescription {get; set;}
     public int ManaCost {get; set;}
     public int DamageMultiplier {get; set;}
+    //public int CooldownTime {get;set;}
+    //private int turnsRemaining;
     
 
     public Ability() {
@@ -20,10 +22,22 @@ public class Ability : IDisposable
         ManaCost = -1;
         DamageMultiplier = 1;
         ClassName = stats.C_Class;
+        //CooldownTime = 0;
     }
+
+    /*public void turnsCooldown() {
+        turnsRemaining = CooldownTurns;
+        if(turnsRemaining > 0) {
+            turnsRemaining--;
+        }
+        else {
+            //can use ability
+        }
+    }*/
 
     public void Dispose(){}
     void Update() {}
     void Start() {
+        //turnsRemaining = 0;
     }
 }
