d7cf5327c194833e6770d4ede9e4535a8a1f74ba
MaxHwoy <mkulb002@ucr.edu>
2023-06-09 15:11:42 -0700

    disable grass for now

--- Assets/Scripts/Map/Tile.cs
+++ Assets/Scripts/Map/Tile.cs
@@ -86,7 +86,9 @@ namespace Project.Map
         {
         }
 
-        public BackgroundInfo(int yValue, TileType type) : base(yValue, type, "Map/PathTiles/" + (type == TileType.Blank ? ("Grass" + Random.Range(1, 17).ToString()) : type.ToString()))
+        //public BackgroundInfo(int yValue, TileType type) : base(yValue, type, "Map/PathTiles/" + (type == TileType.Blank ? ("Grass" + Random.Range(1, 17).ToString()) : type.ToString()))
+
+        public BackgroundInfo(int yValue, TileType type) : base(yValue, type, "Map/PathTiles/" + type.ToString())
         {
         }
 
